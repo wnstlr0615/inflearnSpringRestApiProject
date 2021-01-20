71f268c610dc29429723f58340ddc126b6d195a6
joonsik <ryan0@kakao.com>
2021-01-20 09:18:24 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/joon/demoinflearnrestapi/DemoInflearnRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.joon.demoinflearnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInflearnRestApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInflearnRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joon/demoinflearnrestapi/DemoInflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joon.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInflearnRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
