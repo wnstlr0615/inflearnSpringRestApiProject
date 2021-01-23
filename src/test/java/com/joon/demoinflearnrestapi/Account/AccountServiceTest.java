1447417509af0dfc70ce5e9f75da751968397fb2
wnstlr0615 <ryan0@kakao.com>
2021-01-23 21:46:49 +0900

    32. 예외 테스트

--- src/test/java/com/joon/demoinflearnrestapi/Account/AccountServiceTest.java
+++ src/test/java/com/joon/demoinflearnrestapi/Account/AccountServiceTest.java
@@ -1,11 +1,14 @@
 package com.joon.demoinflearnrestapi.Account;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,6 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+    @Rule
+    public ExpectedException expectedException=ExpectedException.none();
     @Autowired
     AccountService accountService;
     @Autowired
@@ -35,5 +40,11 @@ public class AccountServiceTest {
         UserDetails userDetails=userDetailsService.loadUserByUsername(username);
 
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+    @Test(expected = UsernameNotFoundException.class)
+    public void findByUserNameFail(){
+        String username="ryan@kkk.com";
+        accountService.loadUserByUsername(username);
+
     }
 }
